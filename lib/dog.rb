72e0f39de2e34fe995faa811d68cf5d41a1b913e
Kenneth Valdivia <kenneth_valdivia@yahoo.com>
2020-01-08 02:53:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,7 @@
+class Dog 
+  def initialize(name) 
+    @name = name 
+  def initialize(breed)
+  @breed = breed
+end
+
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person
+  def initialize(name)
+    @name = name
+  end
+  
+  def initialize(first_job, second_job)
+    @first_job = first_job
+    @second_job = second_job
